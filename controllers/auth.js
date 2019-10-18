c485af84922e00526f92e6f31f4759ed8ab82347
GinoAmaury <ginogongora@wposs.com>
2019-10-18 10:20:24 -0500

    Ajuste
    Se ajusta la respuesta

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,12 +20,12 @@ function signUp (req,res){
 
   console.log(user)
 
-  User.findOne({email: req.body.email},(err,user)=>{
+  User.findOne({email: req.body.email},(err,userR)=>{
     if(err) return res.status(500).send({message:err})
-    if(user) return res.status(404).send({message: 'El usuario ya se registro con ese email'})
-    User.findOne({idCard: req.body.idCard},(err,user)=>{
+    if(userR) return res.status(404).send({message: 'El usuario ya se registro con ese email'})
+    User.findOne({idCard: req.body.idCard},(err,userR)=>{
       if(err) return res.status(500).send({message:err})
-      if(user) return res.status(404).send({message: 'El usuario ya se registro con esa cedula'})
+      if(userR) return res.status(404).send({message: 'El usuario ya se registro con esa cedula'})
         user.save((err, userStored)=>{
           if(err) return res.status(500).send({message: `Error al crear el usuario: ${err}`})
           return res.status(200).send({ token: service.createToken(user), user: userStored})
