f89a7f7c493cf2928971adc587f901c60cae7561
GinoAmaury <ginogongora@wposs.com>
2019-10-21 16:08:00 -0500

    Prueba

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,23 @@ const categoryControl = require('../controllers/category')
 const orderControl = require('../controllers/order')
 
 const auth = require('../middlewares/auth')
-const upload = require('../middlewares/upload')
+//const upload = require('../middlewares/upload')
+
+
+const path = require ('path')
+const multer = require ('multer')
+
+let storage = multer.diskStorage ({
+    destination: (req,file,cb)=>{
+        cb(null,'../images')
+    },
+    filename: (req,file,cb)=>{
+        cb(null, file.fieldname +"-"+Date.now()+ path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage})
+
 const api = express.Router()
 api.use(cors())
 
