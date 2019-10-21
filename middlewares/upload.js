a9908574c9be3198170216f801290deba291cf90
GinoAmaury <ginogongora@wposs.com>
2019-10-21 15:01:08 -0500

    AJUSTE
    Se ajusto la ruta de donde se encuentra la carpeta de imagenes

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -7,7 +7,7 @@ const multer = require ('multer')
 
 let storage = multer.diskStorage ({
     destination: (req,file,cb)=>{
-        cb(null,'../images')
+        cb(null,'../../images')
     },
     filename: (req,file,cb)=>{
         cb(null, file.fieldname +"-"+Date.now()+ path.extname(file.originalname))
